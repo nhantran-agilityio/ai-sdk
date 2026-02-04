fb9aab3fbf9c17b7f51374ba38baeb1fb4fe6e99
Nhan Tran <nhan.tran@asnet.com.vn>
2026-02-04 11:17:59 +0700

    feat: apply agent tool weather

--- ai-chatbot-app/app/api/chat/route.ts
+++ ai-chatbot-app/app/api/chat/route.ts
@@ -1,13 +1,31 @@
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  stepCountIs,
+  createUIMessageStream,
+  createUIMessageStreamResponse,
+} from 'ai';
 import { openai } from "@ai-sdk/openai";
+import { getWeather } from '@/lib/ai/tools/get-weather';
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
-  const result = streamText({
-    model: openai("gpt-4o-mini"),
-    messages: await convertToModelMessages(messages),
+  const stream = createUIMessageStream({
+    execute: async ({ writer: dataStream }) => {
+      const result = streamText({
+        model: openai('gpt-4o-mini'),
+        messages: await convertToModelMessages(messages),
+        stopWhen: stepCountIs(5),
+        tools: {
+          getWeather,
+        },
+      });
+
+      dataStream.merge(result.toUIMessageStream());
+    },
   });
 
-  return result.toUIMessageStreamResponse();
+  return createUIMessageStreamResponse({ stream });
 }
